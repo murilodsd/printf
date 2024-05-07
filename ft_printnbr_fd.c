b757a19dc71120ea011656f648ef8d4e900bfbbc
murilodsd <mdsouzadias@hotmail.com>
2024-05-07 09:44:49 -0300

    printnbr done

--- ft_printnbr_fd.c
+++ ft_printnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mde-souz <mde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 13:29:48 by mde-souz          #+#    #+#             */
-/*   Updated: 2024/05/06 20:55:51 by mde-souz         ###   ########.fr       */
+/*   Updated: 2024/05/07 09:43:34 by mde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ static int	ft_putabsnbr_fd(int n, int fd)
 	return (count);
 }
 
-int	ft_printnbr_fd(int nbr, Flag flags, int fd)
+/* int	ft_printnbr_fd2(int nbr, Flag flags, int fd)
 {
 	int	count;
 	int	decplaces;
@@ -84,20 +84,76 @@ int	ft_printnbr_fd(int nbr, Flag flags, int fd)
 	while (flags.digits-- > decplaces)
 			count += ft_putchar_fd('0',1);
 	return (count + ft_putabsnbr_fd(nbr, 1));
+} */
+
+static int	printflags(Flag flags, int nbr)
+{
+	
+	if (nbr < 0)
+		return (ft_putchar_fd('-',1));
+	else if (flags.plus)
+		return (ft_putchar_fd('+',1));
+	else if (flags.space)
+		return (ft_putchar_fd(' ',1));
+	return (0);
+}
+
+static void	printspaces(Flag flags, int nbr, int *p_count)
+{
+	int	digitsandflags;
+	int	decplacesandflags;
+	
+	digitsandflags = flags.digits + (flags.space || flags.plus || (nbr < 0));
+	decplacesandflags = ft_countdec(nbr) + (flags.space || flags.plus || (nbr < 0));
+	if (flags.minus != 1)
+	{
+		while (flags.width - *p_count > decplacesandflags && flags.width > digitsandflags)
+			*p_count += ft_putchar_fd(' ',1);	
+	}
+	else
+	{
+		while (flags.width > *p_count)
+			*p_count += ft_putchar_fd(' ',1);
+	}
+}
+
+int	ft_printnbr_fd(int nbr, Flag flags, int fd)
+{
+	int	count;
+	int	decplaces;
+
+	decplaces = ft_countdec(nbr);
+	count = 0;
+	if (flags.minus != 1)
+		printspaces(flags, nbr, &count);
+	count += printflags(flags, nbr);
+	while (decplaces < flags.digits)
+	{
+		count += ft_putchar_fd('0',1);
+		flags.digits--;
+	}
+	if (!flags.dot || flags.digits != 0 || nbr != 0)
+		count += ft_putabsnbr_fd(nbr, 1);
+	if (flags.minus == 1)
+		printspaces(flags, nbr, &count);
+	return (count);
 }
 #include <stdio.h>
 int	main(void)
 	{
 		Flag	flags;
 
-		int	n = -123;
-		flags.digits = 2;
-		flags.width = 4;
-		flags.space = 0;
+		int	n = 5;
+		flags.digits = 3;
+		flags.width = 7;
+		flags.space = 1;
+		flags.minus = 1;
 		flags.plus = 0;
-		printf("%4.2d\n",n);
+		flags.dot = 1;
+		printf("%d\n",n);
+		printf("% -7.3da\n",n);
 		ft_printnbr_fd(n,flags,1);
-		printf("\n");
+		printf("a\n");
 /* 		printf("\nTESTE PARA 123\n");
 		ft_putnbr_fd(123,1);
 		printf("\nTESTE PARA -15465\n");
